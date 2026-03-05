cddccc6df26347ea1cccb0b65a5fee7b282e41c1
Julia Valkama <juliavalkama2012@gmail.com>
2026-03-05 19:38:28 +0200

    NotesApp Refactored

--- NotesApp/NodeApp_backend/mongo.js
+++ NotesApp/NodeApp_backend/mongo.js
@@ -7,35 +7,30 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.lfm1lul.mongodb.net/noteApp?appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url, { family: 4 })
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minLength: 5,
-    required: true
-  },
-  important: Boolean
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'Mongoose makes things easy',
-  important: true,
-})
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
 // })
 
-Note.find({}).then(result => {
-  result.forEach(note => {
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
     console.log(note)
   })
   mongoose.connection.close()
