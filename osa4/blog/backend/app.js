ba969a8a47f52c4224cad1abf886e0fadce4198b
Julia Valkama <juliavalkama2012@gmail.com>
2026-03-12 17:10:26 +0200

    Exercises 4.18.-4.19.

--- osa4/blog/backend/app.js
+++ osa4/blog/backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -18,5 +19,6 @@ mongoose
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
