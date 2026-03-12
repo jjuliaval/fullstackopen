23477eeda93a4f415b016da988df7c1696f05a90
Julia Valkama <juliavalkama2012@gmail.com>
2026-03-12 15:37:32 +0200

    exercise 4.17

--- osa4/blog/backend/controllers/blogs.js
+++ osa4/blog/backend/controllers/blogs.js
@@ -1,23 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+  .find({}).populate('user', {username: 1, name: 1, id: 1})
+
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findOne({})
 
-  if (!blog.likes) {
-    blog.likes = 0
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
   }
 
-  if (!blog.title || !blog.url) {
+  if (!body.likes) {
+    body.likes = 0
+  }
+
+  if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title or url missing' })
   }
 
+  blog =  new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
   response.status(201).json(result)
 })
 
